b2f68a79b99dfc449fcbcf5a94fc7eace975256c
procamora <pablojoserocamora@gmail.com>
2017-03-15 23:27:23 +0100

    implementado shell para comandos

--- NanoP2P/src/es/um/redes/P2P/App/PeerShell.java
+++ NanoP2P/src/es/um/redes/P2P/App/PeerShell.java
@@ -1,23 +1,74 @@
 package es.um.redes.P2P.App;
 
+import java.util.Scanner;
+
 public class PeerShell implements PeerShellIface {
+
+	private String comando;
+	private String[] argumentos;
+	private Scanner scanner;
+
+	public PeerShell() {
+		scanner = new Scanner(System.in);
+		argumentos = new String[5];// max 5 argumentos
+		comando = new String();
+	}
 
 	@Override
 	public byte getCommand() {
 		// TODO Auto-generated method stub
-		return 0;
+		switch (comando) {
+		case "query":
+			return PeerCommands.COM_QUERY;
+
+		case "download":
+			return PeerCommands.COM_DOWNLOAD;
+
+		case "quit":
+			return PeerCommands.COM_QUIT;
+
+		case "show":
+			return PeerCommands.COM_SHOW;
+
+		case "help":
+			return PeerCommands.COM_HELP;
+
+		default:
+			return PeerCommands.COM_INVALID;
+		}
 	}
 
 	@Override
 	public String[] getCommandArguments() {
 		// TODO Auto-generated method stub
-		return null;
+		return argumentos;
 	}
 
 	@Override
 	public void readCommand() {
 		// TODO Auto-generated method stub
+		System.out.print("[procamora@p2p]# ");
+		String linea = scanner.nextLine();
+		String[] parsea = linea.split(" ");
 
+		for (int i = 0; i < parsea.length; i++) {
+			if (i == 0) // comando
+				comando = parsea[i];
+			else { // argumentos
+					// i-1 para que empiece en 0, ya que el i=0 es el comando
+				argumentos[i - 1] = parsea[i];
+			}
+		}
+
+		//imprimeComando();
 	}
 
+	private void imprimeComando() {
+		System.out.println("inicio comando");
+		System.out.println("comando: " + comando);
+		for (int i = 0; i < argumentos.length; i++)
+			if (argumentos[i] != null)
+				System.out.printf("argumento %d: %s\n", i, argumentos[i]);
+		System.out.println("fin comando");
+	}
 }
---
+++ NanoP2P/src/es/um/redes/P2P/pruebas/shell.java
@@ -0,0 +1,34 @@
+package es.um.redes.P2P.pruebas;
+
+public class shell {
+
+	public static void main(String[] args) {
+
+		String comando = null;
+		String[] argumentos = new String[5];
+
+		String[] parsea = "esto es una prueba".split(" ");
+		for (int i = 0; i < parsea.length; i++) {
+			if (i == 0) // comando
+				comando = parsea[i];
+			else { // argumentos
+
+				argumentos[i - 1] = parsea[i];
+				// i-1 para que empiece en 0, ya que el i=0 es el comando
+			}
+		}
+
+		System.out.println(argumentos);
+
+		System.out.println("inicio comando");
+		System.out.println("comando: " + comando);
+		for (int i = 0; i < argumentos.length; i++)
+			if (argumentos[i] != null)
+				System.out.printf("argumento %d: %s\n", i, argumentos[i]);
+		System.out.println("fin comando");
+		
+	
+
+	}
+
+}
