47810e4dc89b60b697dde4993b30a123cba5b01d
Pablo <pablojoserocamora@gmail.com>
2017-05-04 00:52:22 +0200

    clases de pruebas

--- NanoP2P/src/es/um/redes/P2P/CodigoPruebas/bina.java
+++ NanoP2P/src/es/um/redes/P2P/CodigoPruebas/bina.java
@@ -1,28 +1,22 @@
 package es.um.redes.P2P.CodigoPruebas;
 
 import java.nio.ByteBuffer;
+import es.um.redes.P2P.PeerPeer.Message.*;
 
 public class bina {
 	public static void main(String[] args) {
+		int chunSize = 4096;
+		int tam = 4880;
 
-		byte[] bytes = new byte[10];
-		bytes[0] = (byte) 154;
-		bytes[1] = (byte) 14;
-		
-		ByteBuffer buf = ByteBuffer.wrap(bytes);
-		bytes = new byte[buf.remaining()];
-		buf.get(bytes, 0, bytes.length);
-		buf.clear();
+		System.out.println(tam % chunSize);
 
-		bytes = new byte[buf.capacity()];
+		System.out.println(Long.MAX_VALUE);
 
-		buf.get(bytes, 0, bytes.length);
+		Message m = Message.makeChunkResponseRequest(Long.MAX_VALUE - 1, new byte[5], (short) 4096);
+		Message m2 = Message.makeChunkRequest("87C43258EB02FD2D077B6AC9B4D520D28B49EB04", Long.MAX_VALUE - 1);
+		System.out.println(m.toByteArray());
+		System.out.println(m2.toByteArray());
 		
-		
-		System.out.println(bytes.length);
-		System.out.println(buf);
-		
-
 	}
 
 }
